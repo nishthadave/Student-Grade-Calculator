46366943601813bf489bad6f45d4ee325eadf1a7
nishthadave <111345695+nishthadave@users.noreply.github.com>
2024-07-09 17:40:45 -0400

    Add files via upload

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Number of subjects
+        int numSubjects = 5;  // You can change this number based on your requirements
+        
+        // Array to store marks
+        int[] marks = new int[numSubjects];
+        
+        // Input marks for each subject
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + " (out of 100): ");
+            marks[i] = scanner.nextInt();
+        }
+        
+        // Calculate total marks
+        int totalMarks = 0;
+        for (int mark : marks) {
+            totalMarks += mark;
+        }
+        
+        // Calculate average percentage
+        double averagePercentage = (double) totalMarks / numSubjects;
+        
+        // Calculate grade
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+        
+        // Display results
+        System.out.println("\nTotal Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+        
+        // Close the scanner to prevent resource leak
+        scanner.close();
+    }
+}
